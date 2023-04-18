12bbc777e677985fec732e72179962af2727c9e2
Josh Shepherd <joshuashepherd877@gmail.com>
2023-04-18 17:22:28 +0100

    add tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,11 +2,11 @@
 Character route tests
 .../api/characters/
 '''
-def test_characters(client):
+def test_get_characters(client):
     response = client.get('/api/characters/')
     assert response.status_code == 200
 
-def test_character(client):
+def test_get_character(client):
     response = client.get('api/characters/1')
     assert response.status_code == 200
 
@@ -14,10 +14,10 @@ def test_character(client):
 Episode route tests
 .../api/episodes/
 '''
-def test_episodes(client):
+def test_get_episodes(client):
     response = client.get('/api/episodes/')
     assert response.status_code == 200
 
-def test_episode(client):
+def test_get_episode(client):
     response = client.get('api/episodes/1')
     assert response.status_code == 200
